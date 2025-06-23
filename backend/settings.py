d1975deedb2cb196b70fa14d21ecd7a653d2ee8e
Juliano Ostroski <juliano.ostroski@gmail.com>
2025-06-23 11:49:10 -0400

    refactor: Rename app to 'accounts'

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend.account',
+    'backend.accounts',
 ]
 
 MIDDLEWARE = [
